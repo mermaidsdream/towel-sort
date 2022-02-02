a5c69ab3e9dd2535fb8faefc7de96210e7ffcb2e
Vasylyna Lykhogodenko <vvasylyna@yahoo.com>
2022-02-02 11:04:12 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
-// You should implement your task here.
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let a = matrix.reduce((acc, cur, i) => {
+    cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => acc.push(e));
+    return acc;
+  }, []);
+  return a;
 }
